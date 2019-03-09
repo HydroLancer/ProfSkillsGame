5ded9f13823acd3995d18e00c5d623c7bc1ba9a7
Warren Macaulay Thomas Barnes <WMTBarnes@uclan.ac.uk>
2019-03-09 13:17:41 +0000

    just adding a media folder
    Whoever has the unique models put them in the media folder I think we need to discuss this later what look we are going for on the level

--- ShapeSprint/ShapeSprint.cpp
+++ ShapeSprint/ShapeSprint.cpp
@@ -19,27 +19,24 @@ void main()
 	// Create a 3D engine (using TLX engine here) and open a window for it
 	I3DEngine* myEngine = New3DEngine( kTLX );
 	myEngine->StartWindowed();
-
-	// Load the map file in
+	// Add default folder for meshes and other media
+	myEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
+	myEngine->AddMediaFolder("media");
+	/**** Set up your scene here ****/
+		// Load the map file in
 	CGameMap* map = new CGameMap; // Create a map loader object
 
 	CGameMap::FullLevel level; //2D Vector
 	const string levelName = "level1.txt"; // File name for the map
 	map->LoadTheMap(level, map->startCoods, map->checkpointCoords, map->endCoords, map->timeLimit, map->mapWidth, map->mapHeight, levelName);
-	
-	// Add default folder for meshes and other media
-	myEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
-
-	/**** Set up your scene here ****/
-
 
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
 	{
 		// Draw the scene
 		myEngine->DrawScene();
-
 		/**** Update your scene each frame here ****/
+
 
 	}
 
