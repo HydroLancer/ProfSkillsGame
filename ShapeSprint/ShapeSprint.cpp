4ae0bbbd9ec0e1a3a5fa627f9fb512ceb1cd3d2c
Warren Macaulay Thomas Barnes <WMTBarnes@uclan.ac.uk>
2019-03-09 14:34:43 +0000

    tl engine
    tl engine generate the exe in wrong place

--- ShapeSprint/ShapeSprint.cpp
+++ ShapeSprint/ShapeSprint.cpp
@@ -35,7 +35,6 @@ void main()
 		// Draw the scene
 		myEngine->DrawScene();
 		/**** Update your scene each frame here ****/
-		player->update(myEngine, frameTime, map);
 		frameTime = myEngine->Timer();
 	}
 
